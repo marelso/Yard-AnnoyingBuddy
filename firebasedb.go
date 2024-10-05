eb845119d5d754e5fd5ba265eebb6fe1cbc5e6af
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-05 02:25:36 -0300

    Fixing database connection

--- firebasedb.go
+++ firebasedb.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/db"
+	"fmt"
 	"google.golang.org/api/option"
 )
 
@@ -17,12 +15,8 @@ type Firebase struct {
 var database Firebase
 
 func (db *Firebase) Connect() error {
-	home, err := os.Getwd()
-	if err != nil {
-		return err
-	}
 	ctx := context.Background()
-	opt := option.WithCredentialsFile(home + "healthier-19122-firebase-adminsdk.json")
+	opt := option.WithCredentialsFile("healthier-19122-firebase-adminsdk.json")
 	config := &firebase.Config{DatabaseURL: "https://healthier-19122-default-rtdb.firebaseio.com/"}
 	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
