6f8856144cbc3e982831fe8aeb967a88fc339174
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-05 05:08:32 -0300

    Doing use of new initialization method and starting threads to send pushes

--- main.go
+++ main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
-  "fmt"
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	err := connect()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+	for {
+		var wg sync.WaitGroup
+		data, err := fetchDevices(context.Background(), fb.database)
+		if err != nil {
+			log.Fatalf("Failed to fetch entries: %v\n", err)
+		}
+
+		for id, device := range data.List {
+			wg.Add(1)
+			go func(deviceID string, d Device) {
+				defer wg.Done()
+				processDevice(deviceID, d)
+			}(id, device)
+		}
+
+		wg.Wait()
+		fmt.Println("All devices processed.")
+
+		time.Sleep(60 * time.Second)
+	}
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func processDevice(id string, device Device) {
+	fmt.Printf("Now processing device : %s\n", id)
+	SendPush("Title", "Message body", "cT9UlTL-TgaHjBH518zyVH:APA91bE85WIpjriKUSWolNwjAXySykoTXhlJ_uTsTxszssAvjqPWmRB9seB9ijGbXl7K0F5FPHScE7PynAqG6FVmzobbtZDAMnQl_TLmrfmPsMcNZmlB-w5OBURs5ro_lSXnqMmMHDYO")
+}
